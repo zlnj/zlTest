c1d0695458f3ab7938f1dbf93f01490e500c09de
zhanglulu <zhanglulu@mychebao>
2017-06-03 17:33:32 +0800

    bug解决

--- zllTest/src/main/java/com/zll/test/Main.java
+++ zllTest/src/main/java/com/zll/test/Main.java
@@ -9,5 +9,10 @@ public class Main
     {
         System.out.println("Hello World");
         System.out.println("ss");
+        System.out.println("ss");
+        System.out.println("ss");
+        System.out.println("ss");
+        System.out.println("ss");
+        System.out.println("ss");
     }
 }
