e2a22f1df3eeb09222cd4112037c9ecf35409e4d
zhanglulu <zhanglulu@mychebao>
2017-06-03 16:56:56 +0800

    ttr

---
+++ zllTest/src/main/java/com/zll/test/Main.java
@@ -0,0 +1,12 @@
+package com.zll.test;
+
+/**
+ * Created by Administrator on 2017/6/3.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
